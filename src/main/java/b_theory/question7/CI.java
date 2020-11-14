08a2d1b4b8dcb31caee992f6969b8186e9988566
Kristjan Kõiv <kristjankv32@gmail.com>
2020-11-14 15:17:21 +0200

    #34 Mandatory theory part 7

--- src/main/java/b_theory/question7/CI.java
+++ src/main/java/b_theory/question7/CI.java
@@ -4,28 +4,29 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: In a general IT context, it is the act of connecting several smaller systems into a larger system.
+    // In the context of developing, it is the act of combining a developers changes (to the code) with the code in the central repository of a project.
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer: When integration is done often and automatic tests are run after integration.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
-    // [] test
+    // [X] compile
+    // [X] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 When bugs emerge, they are caused by a small set of changes, making them easier to track down.
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Setting up tests and/or an automated build system takes time, which means that in a small project, it might not be worth it.
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 solano ci
+    // 2 bitbucket pipelines
 }
