2da6c96e6d84ecfcc519e8143eac354563a336a3
Kristjan Kõiv <kristjankv32@gmail.com>
2020-11-14 16:07:04 +0200

    #34 Halfway through question 8

--- src/main/java/b_theory/question8/CD.java
+++ src/main/java/b_theory/question8/CD.java
@@ -4,17 +4,21 @@ public class CD {
 
     //todo A
     // What is delivery?
-    // Answer:
+    // Answer: "Delivery" as a standalone term isn't usually defined in the same context as "continuous delivery" or "continuous deployment".
+    // When talking about getting a new application or changes to an application to the hands of customer, then "deploy" is usually the term that is used.
+    // When answering the rest of question 8 I will use the terms "delivery" and "deployment" to both refer to what was described in the previous sentence.
 
     //todo B
     // What makes delivery continuous?
-    // Answer:
+    // Answer: Automation of the delivery process
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick which ones stand for continuous delivery
     // [] compile
     // [] test
     // [] deploy
+    // None of them, although compile and test are parts of continuous integration, which is pretty much a prerequisite for continuous delivery
+    // Using which definition of "continuous delivery"???
 
     //todo D
     // Name and explain 1 benefit of CD
