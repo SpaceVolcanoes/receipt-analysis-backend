15fd561932202697f9a1356e214f35e9469c2053
Kristjan Koitla <koitlakristjan@gmail.com>
2020-11-21 21:46:52 +0200

    #34 Restore part a theory

---
+++ src/main/java/a_theory/question3/Cat.java
@@ -0,0 +1,14 @@
+package a_theory.question3;
+
+/**
+ * Just a feline of the domestic variety.
+ */
+public class Cat {
+
+    protected String greet = "meow";
+
+    public String greetHuman() {
+        return greet;
+    }
+
+}
---
+++ src/main/java/a_theory/question4/DependencyInjection.java
@@ -0,0 +1,43 @@
+package a_theory.question4;
+
+public class DependencyInjection {
+
+    //todo
+    // One of the reasons we use Spring is that it gives us good support for
+    // Dependency Injection (DI) and Inversion of Control (IoC)
+
+    //todo p1
+    // In your words (do not use wiki definitions)
+    // What is Dependency Injection?
+    // Answer: Passing specific dependencies (other objects or configuration properties) that our objects need to
+    // our objects during their creation either via constructor, field or method injection.
+
+    //todo p2
+    // Bring example from your code of Dependency Injection.
+    // Paste your code here, but comment it out
+
+    // Constructor injection example from package ee.taltech.receipt.service;
+
+    // @Service
+    // public class FileSystemStorageService implements StorageService {
+    //
+    // private final Path rootLocation;
+    //
+    // @Autowired
+    // public FileSystemStorageService(StorageProperties properties) {
+    //      this.rootLocation = Paths.get(properties.getLocation());
+    // }
+
+    //todo p3
+    // Name 2 benefits of Dependency Injection
+    // 1 - We don't need to worry about manually creating our objects and having all of their dependencies also
+    //     at hand in the scope of where we would otherwise want to create those objects.
+    // 2 - We can replace a specific dependency with one which has a matching interface through out our project
+    //     in one place without having go find all the different places where that dependency was in use.
+
+    //todo p4
+    // Name 1 disadvantage of Dependency Injection
+    // 1 - It comes with a learning curve and handling edge cases may initially take a lot more time. For example
+    //     if most of the classes want a new instance of ServiceA but 2 special classes want the same single
+    //     special ServiceA.
+}
