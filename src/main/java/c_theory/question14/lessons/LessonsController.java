2dfbf258823c281cc33fb0346e896004a266e04f
Kristjan Kõiv <kristjankv32@gmail.com>
2021-01-06 14:56:23 +0200

    #44 Krikoi theory update.

--- src/main/java/c_theory/question14/lessons/LessonsController.java
+++ src/main/java/c_theory/question14/lessons/LessonsController.java
@@ -1,5 +1,8 @@
 package c_theory.question14.lessons;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RequestMapping("lessons")
@@ -35,6 +39,10 @@ public class LessonsController {
 
     // [X] todo B create a method to query lessons (plural)
     @GetMapping("")
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "List of lessons"),
+    })
+    @ApiOperation(value = "Get a list of Lessons", produces = "application/json")
     public ResponseEntity<?> getLessons(
         @RequestParam(defaultValue = "") Long courseId,
         @RequestParam(defaultValue = "") Integer year,
@@ -45,36 +53,65 @@ public class LessonsController {
 
     // [X] todo C create a method to query single lesson
     @GetMapping("{id}")
-    public ResponseEntity<?> getLesson(@PathVariable Long id) {
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "Lesson found"),
+        @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "No such Lesson"),
+    })
+    @ApiOperation(value = "Get Lesson by url", produces = "application/json")
+    public ResponseEntity<?> find(@PathVariable Long id) {
         return new ResponseEntity<>(new Lesson(), HttpStatus.OK);
     }
 
     // [X] todo D create a method to save a lesson
     @PostMapping("")
-    public ResponseEntity<?> addLesson(@RequestBody Lesson lesson) {
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_CREATED, message = "Lesson created successfully")
+    })
+    @ApiOperation(value = "Create a new Lesson", produces = "application/json")
+    public ResponseEntity<?> create(@RequestBody Lesson lesson) {
         return new ResponseEntity<>(lesson, HttpStatus.OK);
     }
 
     // [X] todo E create a method to update a lesson
     @PutMapping("{id}")
-    public ResponseEntity<?> updateLesson(@RequestBody Lesson lesson, @PathVariable Long id) {
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "Lesson updated"),
+        @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "No such Lesson")
+    })
+    @ApiOperation(value = "Update a Lesson", produces = "application/json")
+    public ResponseEntity<?> update(@RequestBody Lesson lesson, @PathVariable Long id) {
         return new ResponseEntity<>(lesson, HttpStatus.OK);
     }
 
     // [X] todo F create a method to delete a lesson
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteLesson(@PathVariable Long id) {
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "Lesson deleted"),
+        @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "No such Lesson"),
+    })
+    @ApiOperation(value = "Delete Lesson by id")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         return ResponseEntity.ok().build();
     }
 
     // [X] todo G assuming each Lesson has students (one-to-many relation) create a method to query lesson's students
     @GetMapping("{id}/students")
-    public ResponseEntity<?> getStudents(@PathVariable Long id) {
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "Lesson found"),
+        @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "No such Lesson"),
+    })
+    @ApiOperation(value = "Find all students in a lesson")
+    public ResponseEntity<?> students(@PathVariable Long id) {
         return new ResponseEntity<>(List.of(), HttpStatus.OK);
     }
 
     // [X] todo H create a method to update lesson's name (and nothing else)
     @PutMapping("{id}/name")
+    @ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_OK, message = "Lesson name updated"),
+        @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "No such lesson"),
+    })
+    @ApiOperation(value = "Update lesson's name")
     public ResponseEntity<?> updateName(@PathVariable Long id, @RequestParam(defaultValue = "") String name) {
         // Return the lesson, whose name was updated
         return new ResponseEntity<>(new Lesson(), HttpStatus.OK);
