95a94438e6647eaf401c5e5f0f9bd6cad188d4ed
Jaak Kütt <jaak.kytt@gmail.com>
2023-02-16 08:18:34 +0200

    Remove obsolete homework related theory questions

--- src/main/java/c_theory/question14/blogs/Author.java
+++
@@ -1,7 +0,0 @@
-package c_theory.question14.blogs;
-
-public class Author {
-    private String displayName;
-    private String realName;
-    private Integer age;
-}
--- src/main/java/c_theory/question14/phones/App.java
+++
@@ -1,9 +0,0 @@
-package c_theory.question14.phones;
-
-import java.time.LocalDate;
-
-public class App {
-    private String name;
-    private String author;
-    private LocalDate latestUpdate;
-}
